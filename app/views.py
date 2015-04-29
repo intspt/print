308a0be0752db798889d11c47a46940193937cd3
intspt <intspt@qq.com>
2015-04-29 17:42:13 +0800

    update

--- app/views.py
+++ app/views.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+import time
+
 from app import app, db, lm
 from flask import render_template
+from flask.ext.login import current_user
+
+@app.before_request
+def before_request():
+    g.user = current_user
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+	if current_user.is_authenticated():
+		return render_template('admin.html')
+	else:
+		return render_template('index.html')
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+	pass
+
