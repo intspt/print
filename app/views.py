7cc4b099d30b266a89ae29c57faca370614e4119
wendy2003888 <wendyh2003888@gmail.com>
2015-04-29 22:07:05 +0800

    finish login, edit User in models.py new file:   app/templates/login.html modified:   app/views.py

--- app/views.py
+++ app/views.py
@@ -4,8 +4,13 @@
 import time
 
 from app import app, db, lm
-from flask import render_template, g
-from flask.ext.login import current_user
+from flask import render_template, g, request, redirect
+from flask.ext.login import current_user, login_user, logout_user
+from models import User
+
+@lm.user_loader
+def load_user(userid):
+    return User.query.get(userid)
 
 @app.before_request
 def before_request():
@@ -13,12 +18,26 @@ def before_request():
 
 @app.route('/')
 def home():
-	if current_user.is_authenticated():
-		return render_template('admin.html')
-	else:
-		return render_template('index.html')
+    if current_user.is_authenticated() and current_user.is_admin():
+        return render_template('admin.html')
+    else:
+        return render_template('index.html')
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-	pass
+    if request.method == 'POST':
+        print request.form['name'], request.form['password']
+        if request.form['name'] != None and request.form['password'] != None:
+            user = User.query.filter_by(name = request.form['name']).first()
+            login_user(user)
+            return redirect('/')
+        else:
+            return 'name and password must not be None!'
+    else:
+	   return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/')
 
