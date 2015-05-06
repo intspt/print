fa4f4d57f10938bbe6a38805b0cbdd1a1c898a4f
wendy2003888 <wendyh2003888@gmail.com>
2015-05-06 20:21:54 +0800

    edit by senyu

--- app/views.py
+++ app/views.py
@@ -60,3 +60,13 @@ def logout():
     logout_user()
     return redirect('/')
 
+
+@app.route('/list')
+@throw_exception
+def list():
+    return render_template('list.html')
+
+@app.route('/addTeam')
+@throw_exception
+def addTeam():
+    return render_template('addteam.html')
