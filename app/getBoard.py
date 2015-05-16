7352e514c68ac06b058170d13e274d6de98b6599
wendy2003888 <wendyh2003888@gmail.com>
2015-05-16 16:39:50 +0800

    almost finish balloon

--- app/getBoard.py
+++ app/getBoard.py
@@ -4,31 +4,33 @@ import re
 import os
 
 BOARD_ADDR = './app/templates/summary.html'
+
 solvelist = []
 
-def getBoard():
+def getBoard(problem_num):
     if not os.path.exists(BOARD_ADDR):
         print 'path error, no summary.html'
-    else:  
+    else:
+        global solvelist
+        solvelist = []
         board = open(BOARD_ADDR, 'r')
         contents = board.read().decode('utf-8')
         pattern = re.compile(r'<tr>(.*?)</tr>', re.S)
         lines = re.findall(pattern, contents)
         pattern = ''
-        for i in range(16):  #根据summary里td标签个数改变
+        for i in range(problem_num + 5):  #summary里td标签除题目个数以外是5个
             pattern += '<td>(.*?)</td>'
         # print pattern
         pattern = re.compile(pattern, re.S)
         for line in lines:
             # print line
             info = re.findall(pattern, line)
-            # record = ['a', 'b']
-            # print record
+            # print info
             if info and info[0][0] != '':
                 # print info, len(info)
                 record = []
                 record = [info[0][1], info[0][2]]   #队伍名和总共解决的题数
-                for i in range(4, 15):
+                for i in range(4, problem_num + 4):
                         record.append(info[0][i][2])
                 # print record
                 solvelist.append(record)
@@ -43,5 +45,7 @@ def getBoard():
 def getSolvelist():
     return solvelist
 
-if __name__ == '__main__':
-    getBoard()
+#测试部分
+# if __name__ == '__main__':
+#     problem_num = raw_input('please input problem number')
+#     getBoard(int(problem_num))
--- app/views.py
+++ app/views.py
@@ -12,6 +12,11 @@ from models import User, Submit
 from config import LOGIN_INFO_ERROR, PERMISSION_ERROR, SUBMIT_SECCESS_INFO
 from getBoard import getBoard, getSolvelist
 
+color = []
+problem_num = 0
+balloon_list = []
+sent_list = []
+
 def get_now_time():
     return time.strftime('%H:%M:%S',time.localtime(time.time()))
 
@@ -141,14 +146,64 @@ def logout():
 def board():
     return render_template('summary.html')
 
-@app.route('/balloon')
+@app.route('/balloon', methods = ['GET', 'POST'])
 @admin_required
 @throw_exception
 def balloon():
-    getBoard()
-    solvelist =  getSolvelist()
-    for record in solvelist:
-        if record[1] != '0':
-            # print record[0].encode('utf-8')
-            team = User.query.filter_by(name = record[0]).first()
-    return render_template('balloon.html')
+    global color, problem_num, balloon_list, sent_list 
+    if request.method == 'POST':
+        problem_num = int(request.form['problem_num'])
+        color = []
+        for i in range(problem_num):
+            color.append([str(i + 1), request.form[str(i + 1)]])
+        for i in range(problem_num):
+            print color[i]
+        return redirect('/balloon')
+    elif problem_num == 0:
+        return render_template('balloon.html', balloonList = [], sent_list = [], problem_num = problem_num, color = [])
+    else:
+        # for i in range(problem_num):
+        #     print color[i]
+        print sent_list
+        getBoard(problem_num)
+        solve_list =  getSolvelist()
+        balloon_list = []
+        cnt = 0
+        for record in solve_list:
+            # print record
+            if record[1] != '0':
+                # print record[0].encode('utf-8')
+                team = User.query.filter_by(name = record[0]).first()
+                for i in range(problem_num):
+                    if record[2 + i] != '-':
+                        message = u'给座位:' + team.location + u' 队伍:' + team.name + u' 送第' + str(i + 1) + u'题 ' + color[i][1] +u'色气球'
+                        # print message.encode('utf-8')
+                        balloon_list.append([message, cnt])
+                        cnt += 1
+                        if cnt > len(sent_list):
+                            sent_list.append(False)
+        # for balloon in balloon_list:
+        #     print balloon
+        return render_template('balloon.html', balloon_list = balloon_list, sent_list = sent_list, problem_num = problem_num, color = color)
+
+@app.route('/resetBalloon')
+@admin_required
+@throw_exception
+def resetBalloon():
+    global color, problem_num, sent_list
+    problem_num = 0
+    color = []
+    sent_list = []
+    return redirect('/balloon')
+
+@app.route('/sendBalloon/<int:idx>')
+@admin_required
+@throw_exception
+def sendBalloon(idx):
+    global sent_list
+    sent_list[idx] = True
+    return redirect('/balloon')
+
+
+
+
