9efde34f0d09924f81bbdf37b3e0ccae00fc01e6
Spt <intspt@qq.com>
2015-05-16 23:07:30 +0800

    update
    update

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ import os
 SECRET_KEY = 'dark flame master'
 
 BASE_DIR = os.getcwd()
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + BASE_DIR + '\\app\\data\\data.db'
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + BASE_DIR + '/app/data/data.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + BASE_DIR + os.sep + 'app' + os.sep + 'data' + os.sep + 'data.db'
 
 LOGIN_INFO_ERROR = u'用户名或密码不正确'
 PERMISSION_ERROR = u'权限不足'
